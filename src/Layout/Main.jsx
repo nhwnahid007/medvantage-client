9802edb9803f90df24377a8fd75cb55de40724a2
nhwnahid007 <nhwnahid@gmail.com>
2024-06-03 12:37:49 +0600

    added footer and error page

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,9 +1,13 @@
 import { Outlet } from "react-router-dom";
+import Footer from "../Pages/Shared/Footer/Footer";
 
 const Main = () => {
   return (
-    <div>
-      <Outlet></Outlet>
+    <div className="flex flex-col min-h-screen">
+      <div className="flex-1">
+        <Outlet />
+      </div>
+      <Footer />
     </div>
   );
 };
